31894bf269e924ce14d945fc0c8216aee25ec735
Cassiano Honorio <honoriocassiano@gmail.com>
2025-06-03 20:04:19 -0300

    Padronização dos comentários no emacs.el

--- .emacs.el
+++ .emacs.el
@@ -1,47 +1,47 @@
 (setq
- gc-cons-threshold 402653184 ; Teoricamente aumenta a velocidade de inicialização
+ gc-cons-threshold 402653184 ;; Teoricamente aumenta a velocidade de inicialização
  gc-cons-percentage 0.6)
 
 (setq kass/custom-script-dir "~/.emacs.d/lisp/")
 (setq kass/emacs-custom-file "custom.el")
 
-(setq custom-file (concat kass/custom-script-dir kass/emacs-custom-file)) ; Arquivo diferenciado para salvar as variáveis customizadas
+(setq custom-file (concat kass/custom-script-dir kass/emacs-custom-file)) ;; Arquivo diferenciado para salvar as variáveis customizadas
 
-(ido-mode 1)         ; Autocomplete
-(ido-everywhere 1)   ; Autocomplete (de novo)
-(menu-bar-mode -1)   ; Remove a barra de menu
-(tool-bar-mode -1)   ; Remove a barra de ferramentas
-(scroll-bar-mode -1) ; Remove a barra de rolagem
-(column-number-mode 1) ; Exibe (linha, coluna) na barra de status
-(blink-cursor-mode -1) ; Cursos para de piscar
-(tooltip-mode -1)      ; Não lembro
-(recentf-mode 1)       ; Não sei
-(savehist-mode 1)      ; Aparentemente salva o histórico, mas não parece funcionar
+(ido-mode 1)         ;; Autocomplete
+(ido-everywhere 1)   ;; Autocomplete (de novo)
+(menu-bar-mode -1)   ;; Remove a barra de menu
+(tool-bar-mode -1)   ;; Remove a barra de ferramentas
+(scroll-bar-mode -1) ;; Remove a barra de rolagem
+(column-number-mode 1) ;; Exibe (linha, coluna) na barra de status
+(blink-cursor-mode -1) ;; Cursos para de piscar
+(tooltip-mode -1)      ;; Não lembro
+(recentf-mode 1)       ;; Não sei
+(savehist-mode 1)      ;; Aparentemente salva o histórico, mas não parece funcionar
 
 (setq-default
- indent-tabs-mode nil) ; Alguma coisa relativa a tabs vs espaços
+ indent-tabs-mode nil) ;; Alguma coisa relativa a tabs vs espaços
 
 (setq
- display-line-numbers-type 'relative ; Números relativos nas linhas
- inhibit-splash-screen 1       ; Sem tela inicial
- inhibit-startup-screen 1      ; (Quase) a mesma merda
- scroll-conservatively 10000   ; Para de pular o cursor quando rola para fora do "campo de visão"
- scroll-margin 3               ; Borda para rolagem vertical
- initial-scratch-message nil   ; Não exibe uma mensagem inicial no buffer *scratch*
- recentf-max-saved-items nil   ; Não sei
- tab-width 4                   ; Tab são 4 espaços
- c-basic-offset 4              ; Mesma merda só que diferente
- make-backup-files nil         ; Não salva arquivos de backup
- scroll-step 1                 ; Scroll do mouse funcional
- display-line-numbers-width 4  ; Quantidade de dígitos reservados para os números das linhas
- split-height-threshold 20     ; Tamanho máximo para uma nova "janela"
- compilation-window-height 15) ; Tamanho da "janela" de compilação
+ display-line-numbers-type 'relative ;; Números relativos nas linhas
+ inhibit-splash-screen 1       ;; Sem tela inicial
+ inhibit-startup-screen 1      ;; (Quase) a mesma merda
+ scroll-conservatively 10000   ;; Para de pular o cursor quando rola para fora do "campo de visão"
+ scroll-margin 3               ;; Borda para rolagem vertical
+ initial-scratch-message nil   ;; Não exibe uma mensagem inicial no buffer *scratch*
+ recentf-max-saved-items nil   ;; Não sei
+ tab-width 4                   ;; Tab são 4 espaços
+ c-basic-offset 4              ;; Mesma merda só que diferente
+ make-backup-files nil         ;; Não salva arquivos de backup
+ scroll-step 1                 ;; Scroll do mouse funcional
+ display-line-numbers-width 4  ;; Quantidade de dígitos reservados para os números das linhas
+ split-height-threshold 20     ;; Tamanho máximo para uma nova "janela"
+ compilation-window-height 15) ;; Tamanho da "janela" de compilação
 
-(add-to-list 'default-frame-alist '(fullscreen . maximized)) ; Maximiza a janela após a inicialização
+(add-to-list 'default-frame-alist '(fullscreen . maximized)) ;; Maximiza a janela após a inicialização
 
-(global-display-line-numbers-mode 1) ; Exibe as linhas
-(global-hl-line-mode 1)              ; Destaque na linha atual
-(global-auto-revert-mode 1)          ; Não pede confirmação para recarregar um arquivo do disco
+(global-display-line-numbers-mode 1) ;; Exibe as linhas
+(global-hl-line-mode 1)              ;; Destaque na linha atual
+(global-auto-revert-mode 1)          ;; Não pede confirmação para recarregar um arquivo do disco
 
 (use-package evil
 
@@ -65,42 +65,42 @@
 (evil-mode 1)
 
 ;; Linux
-;; (set-frame-font "-UKWN-Iosevka Custom-regular-normal-expanded-*-15-*-*-*-m-0-iso10646-1") ; Modo horroroso de especificar a fonte
+;; (set-frame-font "-UKWN-Iosevka Custom-regular-normal-expanded-*-15-*-*-*-m-0-iso10646-1") ;; Modo horroroso de especificar a fonte
 ;; Windows
-(set-frame-font "-outline-Fira Mono-regular-normal-normal-mono-14-*-*-*-c-*-iso10646-1") ; Modo horroroso de especificar a fonte
+(set-frame-font "-outline-Fira Mono-regular-normal-normal-mono-14-*-*-*-c-*-iso10646-1") ;; Modo horroroso de especificar a fonte
 
-(setq split-width-threshold nil) ; Não lembro
+(setq split-width-threshold nil) ;; Não lembro
 
-(global-set-key (kbd "<f9>") 'compile) ; Compila
-(global-set-key (kbd "<f6>") 'dired)   ; Abre um diretório
+(global-set-key (kbd "<f9>") 'compile) ;; Compila
+(global-set-key (kbd "<f6>") 'dired)   ;; Abre um diretório
 (global-set-key (kbd "C-/") 'comment-line)
 
-; C-z é esquisito então eu desabilito no modo evil
+;; C-z é esquisito então eu desabilito no modo evil
 (define-key evil-insert-state-map (kbd "C-z") nil)
 (define-key evil-motion-state-map (kbd "C-z") nil)
 (define-key evil-visual-state-map (kbd "C-z") nil)
 (define-key evil-replace-state-map (kbd "C-z") nil)
 (define-key evil-operator-state-map (kbd "C-z") nil)
-; E no emacs
+;; E no emacs
 (global-set-key (kbd "C-z") nil)
 
-(evil-ex-define-cmd "W" 'evil-write)     ; w ou W salvam
-(evil-ex-define-cmd "E" 'evil-edit)      ; e ou E editam
+(evil-ex-define-cmd "W" 'evil-write)     ;; w ou W salvam
+(evil-ex-define-cmd "E" 'evil-edit)      ;; e ou E editam
 
-; Comandos personalizados do Evil
-(evil-ex-define-cmd "Goyo" 'writeroom-mode)        ; inicia o "writeroom"
-(evil-ex-define-cmd "SessionCd" 'kass/session-cd)  ; modo de "sessão"
+;; Comandos personalizados do Evil
+(evil-ex-define-cmd "Goyo" 'writeroom-mode)        ;; inicia o "writeroom"
+(evil-ex-define-cmd "SessionCd" 'kass/session-cd)  ;; modo de "sessão"
 
 (evil-define-minor-mode-key 'normal 'writeroom-mode (kbd "<up>") 'evil-previous-visual-line)
 (evil-define-minor-mode-key 'normal 'writeroom-mode (kbd "<down>") 'evil-next-visual-line)
 
-; Trocar de janela com as setas do mouse
+;; Trocar de janela com as setas do mouse
 (evil-define-key 'normal 'global (kbd "C-w <up>") 'evil-window-up)
 (evil-define-key 'normal 'global (kbd "C-w <down>") 'evil-window-down)
 (evil-define-key 'normal 'global (kbd "C-w <left>") 'evil-window-left)
 (evil-define-key 'normal 'global (kbd "C-w <right>") 'evil-window-right)
 
-; Shift para mantém a seleção visual
+;; Shift para mantém a seleção visual
 (evil-define-key 'visual 'global (kbd "<") '(lambda ()
                                               (interactive)
                                               (evil-shift-left (region-beginning) (region-end))
@@ -113,19 +113,19 @@
                                               (evil-normal-state)
                                               (evil-visual-restore)))
 
-; Movimentação em com hjkl no modo de inserção
+;; Movimentação em com hjkl no modo de inserção
 (evil-define-key 'insert 'global (kbd "M-k") 'evil-previous-line)
 (evil-define-key 'insert 'global (kbd "M-j") 'evil-next-line)
 (evil-define-key 'insert 'global (kbd "M-l") 'evil-forward-char)
 (evil-define-key 'insert 'global (kbd "M-h") 'evil-backward-char)
 
-; Para manter a consistência com a parte acima
+;; Para manter a consistência com a parte acima
 (evil-define-key 'insert 'global (kbd "M-<up>")    'evil-previous-line)
 (evil-define-key 'insert 'global (kbd "M-<down>")  'evil-next-line)
 (evil-define-key 'insert 'global (kbd "M-<right>") 'evil-forward-char)
 (evil-define-key 'insert 'global (kbd "M-<left>")  'evil-backward-char)
 
-; Funções personalizadas
+;; Funções personalizadas
 (defun kass/session-cd (folder)
   (interactive "DPasta para a sessão: ")
   (setq default-directory folder)
@@ -143,9 +143,9 @@
 (defun kass/load-custom-file (name)
   (load-file (concat kass/custom-script-dir name)))
 
-(kass/load-custom-file kass/emacs-custom-file) ; Carrega o arquivo com as variáveis customizadas pelo Emacs
-(kass/load-custom-file "writeroom-mode.el")    ; Modo writeroom para escrita
-(kass/load-custom-file "ifind-mode.el")        ; Encontra arquivos pelo nome
+(kass/load-custom-file kass/emacs-custom-file) ;; Carrega o arquivo com as variáveis customizadas pelo Emacs
+(kass/load-custom-file "writeroom-mode.el")    ;; Modo writeroom para escrita
+(kass/load-custom-file "ifind-mode.el")        ;; Encontra arquivos pelo nome
 
 (defun ifind ()
   (interactive)
@@ -153,5 +153,5 @@
   (ifind-mode))
 
 (setq
- gc-cons-threshold 16777216 ; Oposto das primeiras linhas
+ gc-cons-threshold 16777216 ;; Oposto das primeiras linhas
  gc-cons-percentage 0.1)
